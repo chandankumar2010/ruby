0f5b36dd0f59cad0fe4bd2b1bcdf32c7a4bfe025
Chandan Kumar <chandankumar2010@gmail.com>
2018-06-11 03:38:06 +0530

    Add test for divider program

---
+++ test_divider.rb
@@ -0,0 +1,11 @@
+require "test/unit"
+require_relative "divider"
+
+class TestDivider < Test::Unit::TestCase
+	def test_basic
+		x=10
+		y=2
+		expected_output = 5
+		assert_equal(expected_output,divider(x,y))
+	end
+end
